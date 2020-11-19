c3745c5eb079cb1be5c16531ea2f3a0250d94d24
Alain99-hub <theforestdeath99@gmail.com>
2020-11-18 23:54:23 -0600

    Exception Tarea

--- src/uaslp/enginering/labs/Main.java
+++ src/uaslp/enginering/labs/Main.java
@@ -2,7 +2,7 @@ package uaslp.enginering.labs;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ItemCollisionException {
         try {
             otro();
         } catch (PhantomCollisionException e) {
@@ -10,14 +10,19 @@ public class Main {
         }
     }
 
-    public static void otro() throws PhantomCollisionException {
+    public static void otro() throws PhantomCollisionException, ItemCollisionException {
         try {
             System.out.println("A0");
-            move(1);
+            move(2);
             System.out.println("A1");
         }  catch (WallCollisionException e) {
             System.out.println("A3");
             e.printStackTrace();
+        }catch (ItemCollisionException e){
+            System.out.println("A6");
+            e.printStackTrace();
+          throw new ItemCollisionException();
+
         } finally {
             System.out.println("A5");
         }
@@ -33,7 +38,7 @@ public class Main {
 
     }
 
-    public static void move(int delta) throws PhantomCollisionException, WallCollisionException {
+    public static void move(int delta) throws PhantomCollisionException, WallCollisionException , ItemCollisionException{
         setPosition(getX() + 1, getY());
 
         if (hasCollisions(Phantom.class) || delta == 1) {
@@ -42,6 +47,9 @@ public class Main {
 
         if (hasCollisions(Wall.class) || delta == 2) {
             throw new WallCollisionException();
+        }
+        if(hasCollisions(Item.class) || delta == 3 ){
+            throw new ItemCollisionException();
         }
 
     }
